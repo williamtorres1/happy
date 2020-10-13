bd27294de811405f952305c7d16d71c439e1e4e8
William Torres <contatowilliamtorres@gmail.com>
2020-10-13 12:53:04 -0300

    Initial steps

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react'
+import './styles/global.css'
+interface TitleProps {
+  text: string
 }
 
-export default App;
+function Title(props: TitleProps) {
+  return <h1>{props.text}</h1>
+}
+
+function App(): JSX.Element {
+  return (
+    <div className="App">
+      <Title text="Hello World" />
+    </div>
+  )
+}
+
+export default App
