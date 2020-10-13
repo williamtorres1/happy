36535feef7599aa13498509297bccfa970cd20f1
William Torres <contatowilliamtorres@gmail.com>
2020-10-13 13:31:58 -0300

    landing page finalized

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,17 +1,31 @@
 import React from 'react'
 import './styles/global.css'
-interface TitleProps {
-  text: string
-}
+import './styles/pages/landing.css'
 
-function Title(props: TitleProps) {
-  return <h1>{props.text}</h1>
-}
+import { FiArrowRight } from 'react-icons/fi'
+
+import logoImg from './images/logo.svg'
 
 function App(): JSX.Element {
   return (
-    <div className="App">
-      <Title text="Hello World" />
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy" />
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong>Caruaru</strong>
+          <span>Pernambuco</span>
+        </div>
+
+        <a href="" className="enter-app">
+          <FiArrowRight size={26} color="rgba(0,0,0,0.6)" />
+        </a>
+      </div>
     </div>
   )
 }
