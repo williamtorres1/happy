1d2a42857a170d31169fc84ba8c3dcc1012daff7
William Torres <contatowilliamtorres@gmail.com>
2020-10-17 11:34:26 -0300

    Day 1 finalized, with mapbox service

--- packages/frontend/src/pages/OrphanagesMap.tsx
+++ packages/frontend/src/pages/OrphanagesMap.tsx
@@ -30,7 +30,10 @@ function OrphanagesMap(): JSX.Element {
         zoom={15}
         style={{ width: '100%', height: '100%' }}
       >
-        <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+        />
       </Map>
 
       <Link to="" className="create-orphanage">
